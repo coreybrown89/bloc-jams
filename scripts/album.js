c7bb0c1c6fe01ac15bbbbf033fd73227e21b513a
coreybrown89 <coreybrown89@gmail.com>
2016-09-08 21:00:01 -0500

    checkpoint-26-playPause: completed

--- scripts/album.js
+++ scripts/album.js
@@ -33,7 +33,7 @@ var createSongRow = function(songNumber, songName, songLength) {
 	// html song row template
 	var template =
 	'<tr class="album-view-song-item">'
-	+ '  <td class="song-item-number">' + songNumber + '</td>'
+	+ '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
 	+ '  <td class="song-item-title">' + songName + '</td>'
 	+ '  <td class="song-item-duration">' + songLength + '</td>'
 	+ '</tr>'
@@ -66,9 +66,32 @@ var setCurrentAlbum = function(album) {
         albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
     }
 };
+// Elements with listeners
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+//Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 
 window.onload = function() {
     setCurrentAlbum(albumPicasso);
+
+    songListContainer.addEventListener('mouseover', function(event){
+    	// Only target individual song rows during event delegation
+    	if (event.target.parentElement.className === 'album-view-song-item') {
+    		// Change the content from the number to the play button's HTML
+    		event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+    	}
+    });
+
+    for (var i = 0; i < songRows.length; i++) {
+    	songRows[i].addEventListener('mouseleave', function(event){
+    		// Selects the first child element (song-item-number)
+    		this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+    	});
+    }
+
+
 };
 
 
