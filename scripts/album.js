a327a84c5d9ecd8da2d818d0536a0f6221fc5a26
coreybrown89 <coreybrown89@gmail.com>
2016-09-20 22:20:45 -0500

    Fix querySelector Bug

--- scripts/album.js
+++ scripts/album.js
@@ -125,12 +125,12 @@ window.onload = function() {
   		 	case 'album-song-button':
   		 	case 'ion-play':
   		 	case 'ion-pause':
-  		 		return findParentByClassName(element, 'song-item-number');
+  		 		return findParentByClassName(element, '.song-item-number');
   		 	case 'album-view-song-item':
-  		 		return element.querySelector('song-item-number');
+  		 		return element.querySelector('.song-item-number');
   		 	case 'song-item-title':
   		 	case 'song-item-duration':
-  		 		return findParentByClassName(element, 'album-view-song-item').querySelector('song-item-number');
+  		 		return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
   		 	case 'song-item-number':
   		 		return element;
   		 	default:
